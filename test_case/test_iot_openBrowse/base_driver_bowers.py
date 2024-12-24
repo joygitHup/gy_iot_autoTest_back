ca560cb0768cae80fb8ace12b2331099324abc0f
joygithub <728061158@qq.com>
2024-12-24 16:30:09 +0800

    adopt linux

--- test_case/test_iot_openBrowse/base_driver_bowers.py
+++ test_case/test_iot_openBrowse/base_driver_bowers.py
@@ -1,14 +1,17 @@
 import allure
 from  selenium import  webdriver
+from selenium.webdriver.chrome.service import Service
 @allure.epic("新能源PC端")
 class driver_basePC_class():
     @classmethod
     def setup_class(self):
+        #为Linux准别
+        service = Service(executable_path='/home/test/workspace/apploadpath/chromedriverPath/chrome-linux64/chromedriver')
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument('--headless')  # 浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
         chrome_options.add_argument('--no-sandbox')  # 解决DevToolsActivePort文件不存在的报错
         chrome_options.add_argument('--disable-dev-shm-usage')  # 可以不加这一条
-        self.driver = webdriver.Chrome(options=chrome_options)
+        self.driver = webdriver.Chrome(options=chrome_options,service=service)
         self.driver.get("http://192.168.20.212:5500/#/home")
         self.driver.maximize_window()
 
