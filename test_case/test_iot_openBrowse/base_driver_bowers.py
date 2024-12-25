a5b83d0d5467f6bb8433699e70fd50a5d255482f
joygithub <728061158@qq.com>
2024-12-25 11:55:29 +0800

    sub test case

--- test_case/test_iot_openBrowse/base_driver_bowers.py
+++ test_case/test_iot_openBrowse/base_driver_bowers.py
@@ -8,14 +8,14 @@ class driver_basePC_class():
     @classmethod
     def setup_class(self):
         #为Linux准备
-        # service = Service(executable_path='/home/apploadpath/chromedriverPath/chrome-linux64')
+        executable_path = Service(executable_path='/home/apploadpath/chromedriverPath/chrome-linux64')
         service = ChromeService(ChromeDriverManager().install())
         #本地测试 用一下代码需要在 webdriver.Chrome()注释掉servervice
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument('--headless')  # 浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
         chrome_options.add_argument('--no-sandbox')  # 解决DevToolsActivePort文件不存在的报错
         chrome_options.add_argument('--disable-dev-shm-usage')  # 可以不加这一条
-        self.driver = webdriver.Chrome(options=chrome_options,service=service)
+        self.driver = webdriver.Chrome(executable_path,options=chrome_options,service=service)
         self.driver.get("http://192.168.20.212:5500/#/home")
         self.driver.maximize_window()
 
