8eedd2ad461466f0c751611c5f405975a450411b
joygithub <728061158@qq.com>
2024-12-25 11:47:23 +0800

    sub test case

--- test_case/test_iot_openBrowse/base_driver_bowers.py
+++ test_case/test_iot_openBrowse/base_driver_bowers.py
@@ -1,12 +1,15 @@
 import allure
 from  selenium import  webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 @allure.epic("新能源PC端")
 class driver_basePC_class():
     @classmethod
     def setup_class(self):
         #为Linux准备
-        service = Service(executable_path='/home/apploadpath/chromedriverPath/chrome-linux64')
+        # service = Service(executable_path='/home/apploadpath/chromedriverPath/chrome-linux64')
+        service = ChromeService(ChromeDriverManager().install())
         #本地测试 用一下代码需要在 webdriver.Chrome()注释掉servervice
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument('--headless')  # 浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
