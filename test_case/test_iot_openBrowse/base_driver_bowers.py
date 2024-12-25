218bf406c02288651dd537ee0fb6a02bcf84de51
joygithub <728061158@qq.com>
2024-12-25 12:22:58 +0800

    adopt linux1

--- test_case/test_iot_openBrowse/base_driver_bowers.py
+++ test_case/test_iot_openBrowse/base_driver_bowers.py
@@ -2,6 +2,7 @@ import allure
 from  selenium import  webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 @allure.epic("新能源PC端")
@@ -9,14 +10,13 @@ class driver_basePC_class():
     @classmethod
     def setup_class(self):
         #为Linux准备
-        executable_path='/home/apploadpath/chromedriverPath/chrome-linux64'
         # service = ChromeService(ChromeDriverManager().install())
         #本地测试 用一下代码需要在 webdriver.Chrome()注释掉servervice
-        chrome_options =Options()
-        chrome_options.add_argument('--headless')  # 浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
-        chrome_options.add_argument('--no-sandbox')  # 解决DevToolsActivePort文件不存在的报错
-        chrome_options.add_argument('--disable-dev-shm-usage')  # 可以不加这一条
-        self.driver = webdriver.Chrome(executable_path,options=chrome_options)
+        options =Options()
+        options.add_argument('--headless')  # 浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
+        options.add_argument('--no-sandbox')  # 解决DevToolsActivePort文件不存在的报错
+        options.add_argument('--disable-dev-shm-usage')  # 可以不加这一条
+        self.driver =Chrome(options=options,executable_path='/home/apploadpath/chromedriverPath/chrome-linux64')
         self.driver.get("http://192.168.20.212:5500/#/home")
         self.driver.maximize_window()
 
