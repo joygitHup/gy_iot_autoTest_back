481bd2dae44128bfe13869308cb332cea7338975
joygithub <728061158@qq.com>
2024-12-25 12:29:35 +0800

    adopt linux3

--- test_case/test_iot_openBrowse/base_driver_bowers.py
+++ test_case/test_iot_openBrowse/base_driver_bowers.py
@@ -12,10 +12,12 @@ class driver_basePC_class():
         #为Linux准备
         # service = ChromeService(ChromeDriverManager().install())
         #本地测试 用一下代码需要在 webdriver.Chrome()注释掉servervice
+        chrome_path="/home/apploadpath/chromedriverPath/chrome-linux64"
         options =Options()
         options.add_argument('--headless')  # 浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
         options.add_argument('--no-sandbox')  # 解决DevToolsActivePort文件不存在的报错
         options.add_argument('--disable-dev-shm-usage')  # 可以不加这一条
+        options.binary_location = chrome_path
         self.driver =webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
         self.driver.get("http://192.168.20.212:5500/#/home")
         self.driver.maximize_window()
