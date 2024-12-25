4bb5bb76567f4e60ef955dfb5a14ca644d56deef
joygithub <728061158@qq.com>
2024-12-25 12:19:41 +0800

    adopt linux

--- test_case/test_iot_openBrowse/base_driver_bowers.py
+++ test_case/test_iot_openBrowse/base_driver_bowers.py
@@ -1,6 +1,7 @@
 import allure
 from  selenium import  webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 @allure.epic("新能源PC端")
@@ -8,10 +9,10 @@ class driver_basePC_class():
     @classmethod
     def setup_class(self):
         #为Linux准备
-        executable_path = Service(executable_path='/home/apploadpath/chromedriverPath/chrome-linux64')
+        executable_path='/home/apploadpath/chromedriverPath/chrome-linux64'
         # service = ChromeService(ChromeDriverManager().install())
         #本地测试 用一下代码需要在 webdriver.Chrome()注释掉servervice
-        chrome_options = webdriver.ChromeOptions()
+        chrome_options =Options()
         chrome_options.add_argument('--headless')  # 浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
         chrome_options.add_argument('--no-sandbox')  # 解决DevToolsActivePort文件不存在的报错
         chrome_options.add_argument('--disable-dev-shm-usage')  # 可以不加这一条
